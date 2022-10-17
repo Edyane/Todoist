361585121a53a69f052fcb205cc584a5ab55ceae
Edyane <edyanealves@gmail.com>
2022-10-17 09:03:56 -0300

    Alterando as cores do projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,7 +35,7 @@ function showTodo(filter) {
 			}
 		});
 	}
-	taskBox.innerHTML = liTag || `<span>You don't have any task here</span>`;
+	taskBox.innerHTML = liTag || `<span>Você não tem nenhuma tarefa!</span>`;
 	let checkTask = taskBox.querySelectorAll(".task");
 	!checkTask.length
 		? clearAll.classList.remove("active")
